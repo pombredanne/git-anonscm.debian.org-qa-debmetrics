a54b729bbacf69fbdb6891b5a6f47d613e586199
Joseph Bisch <joseph.bisch@gmail.com>
2014-05-19 08:11:20 -0400

    Use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='debmetrics',
       version='0.0.1',
+      description='Debian Metrics Portal for retrieving, updating,' +
+      'and display of metrics',
+      url='http://anonscm.debian.org/gitweb/?p=users/josephbisch-guest/' +
+      'debmetrics.git;a=summary',
+      author='Joseph Bisch',
+      author_email='joseph.bisch@gmail.com',
       license='AGPL',
-      py_modules=['debmetrics'],
+      packages=['debmetrics'],
       )
