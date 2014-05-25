77de424ba70c9e53db2ef6e077ce4c6ce9d8ba19
Joseph Bisch <joseph.bisch@gmail.com>
2014-05-25 10:10:38 -0400

    Use shell=False in pull_runner.py

--- debmetrics/pull_runner.py
+++ debmetrics/pull_runner.py
@@ -6,7 +6,7 @@ import subprocess
 for filename in os.listdir('manifests'):
     name, ext = os.path.splitext(filename)
     if ext == '.py' and not name == '__init__':
-        if not subprocess.call('./manifests/'+filename, shell=True):
+        if not subprocess.call('./manifests/'+filename, shell=False):
             print 'success'
         else:
             print 'failure'
