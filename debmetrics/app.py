d9b52fba9d8d9128ddfad7663fa6d81900e896cd
Joseph Bisch <joseph.bisch@gmail.com>
2014-06-25 13:33:07 -0400

    Generalize flask code

--- debmetrics/app.py
+++ debmetrics/app.py
@@ -1,9 +1,11 @@
 #! /usr/bin/python
 
-from flask import Flask, jsonify, request, render_template, send_from_directory
+from flask import (Flask, abort, jsonify, request, render_template,
+                   send_from_directory)
 import os
 import datetime
 from pull_runner import db_fetch
+from models import models
 app = Flask(__name__)
 
 
@@ -23,14 +25,15 @@ def get_graph_name_from_date(t, d):
             date = fileparts[-1]
             if d in date:
                 return date
-    return os.path.splitext(os.listdir('graphs'))[0].split('_')[-1]
+    return os.path.splitext(os.listdir('graphs')[0])[0].split('_')[-1]
 
 
 def get_max_date(t):
     max_date = datetime.datetime.strptime('0010-10-10', '%Y-%m-%d')
     for filename in os.listdir('graphs'):
         fileparts = os.path.splitext(filename)[0].split('_')
-        if fileparts[0] == t and fileparts[1] != 'timeseries':
+        if (fileparts[0] == t and fileparts[-1] != 'timeseries' and
+                fileparts[-1] != 'table'):
             date = datetime.datetime.strptime(fileparts[-1],
                                               '%Y-%m-%d %H:%M:%S.%f')
             if date > max_date:
@@ -42,7 +45,8 @@ def get_min_date(t):
     min_date = datetime.datetime.strptime('9000-10-10', '%Y-%m-%d')
     for filename in os.listdir('graphs'):
         fileparts = os.path.splitext(filename)[0].split('_')
-        if fileparts[0] == t and fileparts[1] != 'timeseries':
+        if (fileparts[0] == t and fileparts[-1] != 'timeseries' and
+                fileparts[-1] != 'table'):
             date = datetime.datetime.strptime(fileparts[-1],
                                               '%Y-%m-%d %H:%M:%S.%f')
             if date < min_date:
@@ -73,53 +77,29 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/sources')
-def sources():
-    table = 'vcs'
-    rows, cols = db_fetch(table)
-    graphs, names, timeseries = graph_helper('vcs')
-    return render_template('metric.html', title='sources', graph=graphs[0],
-                           name=names[0], timeseries=timeseries,
-                           headers=cols, rows=rows)
+@app.route('/<metric>')
+def metric(metric):
+    if metric in models:
+        rows, cols = db_fetch(metric)
+        graphs, name, timeseries = graph_helper(metric)
+        return render_template('metric.html', title=metric, graph=graphs[0],
+                               name=name[0], timeseries=timeseries,
+                               headers=cols, rows=rows)
+    else:
+        abort(404)
 
 
-@app.route('/_sources')
-def _sources():
+@app.route('/_<metric>')
+def _sources(metric):
     d = request.args.get('d', type=str)
-    return jsonify(graph=get_graph_from_date('vcs', d),
-                   name=get_graph_name_from_date('vcs', d))
+    return jsonify(graph=get_graph_from_date(metric, d),
+                   name=get_graph_name_from_date(metric, d))
 
 
-@app.route('/_sourcesminmax')
-def _sourcesminmax():
-    return jsonify(maxDate=get_max_date('vcs'),
-                   minDate=get_min_date('vcs'))
-
-
-@app.route('/releases')
-def releases():
-    table = 'releases'
-    rows, cols = db_fetch(table)
-    graphs, names, timeseries = graph_helper('releases')
-    return render_template('metric.html', title='releases', graph=graphs[0],
-                           name=names[0], timeseries=timeseries,
-                           headers=cols, rows=rows)
-
-
-@app.route('/releases_count')
-def releases_count():
-    table = 'releases_count'
-    rows, cols = db_fetch(table)
-    graphs, names, timeseries = graph_helper('releases_count')
-    return render_template('metric.html', title='releases_count',
-                           graph=graphs[0], name=names[0],
-                           timeseries=timeseries,
-                           headers=cols, rows=rows)
-
-
-@app.route('/rc_bugs')
-def rc_bugs():
-    return render_template('rc_bugs.html')
+@app.route('/_<metric>minmax')
+def _sourcesminmax(metric):
+    return jsonify(maxDate=get_max_date(metric),
+                   minDate=get_min_date(metric))
 
 
 @app.route('/graphs/<path:filename>')
--- debmetrics/static/js/global.js
+++ debmetrics/static/js/global.js
@@ -3,3 +3,27 @@ $('#tabs').tabs();
 $('.tablesorter').tablesorter();
 
 $('table').stickyTableHeaders();
+
+var metric = $('h1').text();
+
+$(function() {
+    $('input#date').bind('change', function() {
+        $.getJSON($SCRIPT_ROOT + '/_' + metric, {
+            d: $('input#date').val()
+        }, function(data) {
+            $('#graph').attr('src', data.graph);
+            $('.title').each(function() {$(this).text(data.name)});
+        });
+        return false;
+    });
+});
+
+$.getJSON($SCRIPT_ROOT + '/_' + metric + 'minmax', {},
+        function(data) {
+            $('input#date').datepicker('option', 'maxDate', data.maxDate);
+            $('input#date').datepicker('option', 'minDate', data.minDate);
+        });
+
+$('input#date').datepicker({
+    dateFormat: "yy-mm-dd",
+});
