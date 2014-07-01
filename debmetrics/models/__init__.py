f62653ca3a9b69a8a05f157f8632c05ded0afd5f
Joseph Bisch <joseph.bisch@gmail.com>
2014-07-01 10:11:31 -0400

    Update tests

--- debmetrics/models/__init__.py
+++ debmetrics/models/__init__.py
@@ -12,7 +12,12 @@ for i in (i[:-3] for i in glob1(dirname(__file__), '*.py')):
                                 fromlist=[i])
             models[i] = getattr(module, i.title().replace('_', ''))
         except Exception as err:
-            if log.level < logging.INFO:
-                log.debug("cannot initialize '%s' model", i, exc_info=True)
-            else:
-                log.debug("cannot initialize '%s' model: %s", i, err)
+            try:
+                module = __import__('debmetrics.models.%s' % i,
+                                    fromlist=[i])
+                models[i] = getattr(module, i.title().replace('_', ''))
+            except Exception as err2:
+                if log.level < logging.INFO:
+                    log.debug("cannot initialize '%s' model", i, exc_info=True)
+                else:
+                    log.debug("cannot initialize '%s' model: %s", i, err2)
