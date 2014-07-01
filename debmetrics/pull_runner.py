c3e085c7df9aa8103dc44f27394c261dc59f09c5
Joseph Bisch <joseph.bisch@gmail.com>
2014-07-01 09:03:04 -0400

    Make pull_runner.py only run for pull metrics and graph pull metrics

--- debmetrics/pull_runner.py
+++ debmetrics/pull_runner.py
@@ -33,20 +33,22 @@ def run():
         if ext == '.py' and not name == '__init__':
             manifest = ConfigParser.RawConfigParser()
             manifest.read(os.path.join(man_dir, name + '.manifest'))
-            print os.path.join(man_dir, name + '.manifest')
-            format = manifest.get('script1', 'format')
-            if should_run(name+ext, manifest.get('script1', 'freq')):
-                try:
-                    output = subprocess.check_output(os.path.join(directory,
-                                                                  filename))
-                    if format == 'csv':
-                        header, rows = handle_csv(output)
-                    db_insert(header, rows, name)
-                except subprocess.CalledProcessError:
-                    logger.error('Failure calling process for pull script %s',
-                                 filename)
-            else:
-                logger.error('Did not run pull script %s', filename)
+            t = manifest.get('script1', 'type')
+            if t == 'pull':
+                print os.path.join(man_dir, name + '.manifest')
+                format = manifest.get('script1', 'format')
+                if should_run(name+ext, manifest.get('script1', 'freq')):
+                    try:
+                        output = subprocess.check_output(os.path.join(
+                            directory, filename))
+                        if format == 'csv':
+                            header, rows = handle_csv(output)
+                        db_insert(header, rows, name)
+                    except subprocess.CalledProcessError:
+                        msg = 'Failure calling process for pull script %s'
+                        logger.error(msg, filename)
+                else:
+                    logger.error('Did not run pull script %s', filename)
 
     for filename in os.listdir(graph_scripts_directory):
         name, ext = os.path.splitext(filename)
@@ -56,17 +58,20 @@ def run():
                 data, cols = db_fetch(table)
                 config = ConfigParser.RawConfigParser()
                 config.read(os.path.join(man_dir, table + '.manifest'))
+                t = config.get('script1', 'type')
                 graph_type = config.get('script1', 'graph_type')
-                if data:
-                    if graph_type == 'default':
-                        time_series_graph(table, data, cols)
-                data = pack(data)
-                proc = subprocess.Popen([os.path.join(graph_scripts_directory,
-                                        filename)], stdin=subprocess.PIPE)
-                out, err = proc.communicate(data)
-                if err:
-                    logger.error('Failure with graph script for %s: %s',
-                                 table, err)
+                if t == 'pull':
+                    if data:
+                        if graph_type == 'default':
+                            time_series_graph(table, data, cols)
+                    data = pack(data)
+                    proc = subprocess.Popen([os.path.join(
+                                            graph_scripts_directory,
+                                            filename)], stdin=subprocess.PIPE)
+                    out, err = proc.communicate(data)
+                    if err:
+                        logger.error('Failure with graph script for %s: %s',
+                                     table, err)
             except subprocess.CalledProcessError:
                 logger.error('Failure calling process for graph script for %s',
                              table)
