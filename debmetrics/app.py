f77dcf740e375986d800ff1e0b2d35974f942a26
Joseph Bisch <joseph.bisch@gmail.com>
2014-07-03 14:26:56 -0400

    Encode data POSTed to /push as ascii

--- debmetrics/app.py
+++ debmetrics/app.py
@@ -154,10 +154,10 @@ def _sourcesminmax(metric):
 @app.route('/push', methods=['POST'])
 def push():
     """A route to push data for a push metric."""
-    table = request.form['metric']
-    data = request.form['data']
-    format = request.form['format']
-    token = request.form['token']
+    table = request.form['metric'].encode('ascii')
+    data = request.form['data'].encode('ascii')
+    format = request.form['format'].encode('ascii')
+    token = request.form['token'].encode('ascii')
     if format == 'csv':
         header, rows = handle_csv(data)
     if token_matches(table, token) and store(table, header, rows):
