48c8488f153afc01605f3a8e3434163bc9aed87c
Joseph Bisch <joseph.bisch@gmail.com>
2014-07-07 10:06:53 -0400

    Add manifest2index.py

---
+++ debmetrics/manifest2index.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+"""This modules generates an index file from the manifest file names."""
+
+import sys
+import ntpath
+
+
+def manifest2index(manifests):
+    """Outputs the index file given a list of manifest files.
+
+    Keyword arguments:
+    manifests -- the location of manifests on the filesystem
+    """
+    for ind, manifest in enumerate(manifests):
+        manifests[ind] = ntpath.basename(manifest).split('.', 1)[0]
+    manifests.sort()
+    print """<!doctype html>
+<html lang=en>
+    <head>
+        <meta charset=utf-8>
+        <title>Debian Metrics Portal</title>
+    </head>
+    <body>
+        <h1>Debian Metrics Portal</h1>"""
+    for manifest in manifests:
+        print '        <p><a href="{{ url_for(\'metric\', metric=\'%s\' }}">%s</a></p>' \
+              % (manifest, manifest)
+    print """    </body>
+</html>"""
+
+
+if __name__ == '__main__':
+    sys.argv = sys.argv[1:]
+    manifest2index(sys.argv)
