586271bb51d37020a0c030ff0ee3b0e11f959ee6
Joseph Bisch <joseph.bisch@gmail.com>
2014-07-24 13:29:01 -0400

    Add dpkg_formats metric

---
+++ debmetrics/pull_scripts/dpkg_formats.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+
+import sys
+import csv
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+
+def run():
+    url = 'http://upsilon.cc/~zack/stuff/dpkg-v3/'
+    soup = BeautifulSoup(urllib2.urlopen(url))
+    rows = soup.findAll('td')
+    data = []
+    ts = soup.findAll('tt')[0].string
+    data.append(ts)
+    for row in rows:
+        data.append(row.string)
+    writer = csv.writer(sys.stdout)
+    writer.writerow(['ts', '1.0', '3.0 (native)', '3.0 (quilt)'])
+    writer.writerow(data)
+    sys.exit(0)
+
+if __name__ == '__main__':
+    run()
