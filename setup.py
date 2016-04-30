acaaa5f177008582355795ae22ec51919703bcef
Joseph Bisch <joseph.bisch@gmail.com>
2016-04-30 13:02:46 -0400

    Add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    install_requires = [l for l in f.read().splitlines()
+                        if not l.startswith('#')]
+
 setup(name='debmetrics',
       version='0.0.1',
+      install_requires=install_requires,
       description='Debian Metrics Portal for retrieving, updating,' +
       'and display of metrics',
       url='http://anonscm.debian.org/cgit/qa/debmetrics.git/',
