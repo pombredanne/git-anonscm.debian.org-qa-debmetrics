5bbaf25f7eacb7774241636d8f73ccb5e58a1258
Joseph Bisch <joseph.bisch@gmail.com>
2016-05-04 15:02:48 -0400

    Fix create_all.py for flask_sqlalchemy

--- create_all.py
+++ create_all.py
@@ -1,7 +1,9 @@
 """This module creates all the tables from models directory unless the
 the table already exists"""
 
+from debmetrics.app import app
 from debmetrics.database import db
 from debmetrics.models import models
 
-db.create_all()
+with app.app_context():
+    db.create_all()
